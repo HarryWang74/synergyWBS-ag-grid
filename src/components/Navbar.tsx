85a5b20bff9e3b191a2d3a06aed20bb94c5332b8
Gang Wang <gang.wang@totalsynergy.com>
2025-05-08 20:04:18 +1000

    rename tab

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -41,7 +41,7 @@ const Navbar = () => {
             ' inline-block p-4 hover:bg-i-hover-color'
           }
         >
-          Demo
+          Juan made by claude code
         </NavLink>
       </div>
     </div>
