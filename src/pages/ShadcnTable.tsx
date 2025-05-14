807652924ef5da23a04986af91b16d9a9e6ff1f1
Gang Wang <gang.wang@totalsynergy.com>
2025-05-14 12:13:00 +1000

    Update ShadcnTable.tsx

--- src/pages/ShadcnTable.tsx
+++ src/pages/ShadcnTable.tsx
@@ -3,14 +3,12 @@ import { BsCaretRightFill } from 'react-icons/bs'
 import { BsCaretDownFill } from 'react-icons/bs'
 import {
   Column,
-  Cell,
   ExpandedState,
   useReactTable,
   getCoreRowModel,
   getExpandedRowModel,
   ColumnDef,
   flexRender,
-  Header,
 } from '@tanstack/react-table'
 import { getData } from '../data/shadcn-table-data'
 import './table.css'
@@ -31,11 +29,7 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from '@/components/ui/pagination'
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar"
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
@@ -44,28 +38,6 @@ import {
 } from '@/components/ui/dropdown-menu'
 import { Button } from '@/components/ui/button'
 import { ChevronDown } from 'lucide-react'
-
-// needed for table body level scope DnD setup
-import {
-  DndContext,
-  KeyboardSensor,
-  MouseSensor,
-  TouchSensor,
-  closestCenter,
-  type DragEndEvent,
-  useSensor,
-  useSensors,
-} from '@dnd-kit/core'
-import { restrictToHorizontalAxis } from '@dnd-kit/modifiers'
-import {
-  arrayMove,
-  SortableContext,
-  horizontalListSortingStrategy,
-} from '@dnd-kit/sortable'
-
-// needed for row & cell level scope DnD setup
-import { useSortable } from '@dnd-kit/sortable'
-import { CSS } from '@dnd-kit/utilities'
 
 export const formatCurrency = (amount: number | null) => {
   const value = amount || 0
@@ -83,44 +55,11 @@ export const formatDate = (date: Date) => {
   }).format(date)
 }
 
-const TableHeaderWapper = ({
-  header,
-}: {
-  header: Header<any, unknown>
-}) => {
-  const { attributes, isDragging, listeners, setNodeRef, transform } =
-    useSortable({
-      id: header.column.id,
-    })
-
-  const style: CSSProperties = {
-    opacity: isDragging ? 0.8 : 1,
-    position: 'relative',
-    transform: CSS.Translate.toString(transform), // translate instead of transform to avoid squishing
-    transition: 'width transform 0.2s ease-in-out',
-    whiteSpace: 'nowrap',
-    width: header.column.getSize(),
-    zIndex: isDragging ? 1 : 0,
-  }
-
-  return (
-    <TableHead colSpan={header.colSpan} ref={setNodeRef} style={style}>
-      {header.isPlaceholder
-        ? null
-        : flexRender(header.column.columnDef.header, header.getContext())}
-      <button {...attributes} {...listeners}>
-        🟰
-      </button>
-    </TableHead>
-  )
-}
-
 const ShadcnTable = () => {
   const columns = React.useMemo<ColumnDef<any>[]>(
     () => [
       {
         accessorKey: 'wbs',
-        id: 'wbs', // must have to support drag & drop
         header: ({}) => <>WBS</>,
         cell: ({ row, getValue }) => (
           <div
@@ -156,33 +95,28 @@ const ShadcnTable = () => {
       {
         accessorKey: 'name',
         header: () => 'Name',
-        id: 'name', 
         size: 180,
       },
       {
         accessorKey: 'status',
         header: () => 'Status',
-        id: 'status', 
         size: 180,
       },
       {
         accessorKey: 'startDate',
         cell: ({ getValue }) => formatDate(getValue<Date>() ?? new Date()),
         header: () => 'Start Date',
-        id: 'startDate',
         size: 180,
       },
       {
         accessorKey: 'endDate',
         cell: ({ getValue }) => formatDate(getValue<Date>() ?? new Date()),
         header: () => 'End Date',
-        id: 'endDate',
         size: 180,
       },
       {
         accessorKey: 'assigned',
         header: () => 'Assigned',
-        id: 'assigned',
         cell: ({ getValue }) => {
           const assignee = getValue<any>()
           return (
@@ -199,46 +133,39 @@ const ShadcnTable = () => {
       {
         accessorKey: 'discipline',
         header: () => 'Discipline',
-        id: 'discipline',
         size: 180,
       },
       {
         accessorKey: 'units',
         header: () => 'Units',
-        id: 'units',
         size: 180,
       },
       {
         accessorKey: 'rate',
         cell: ({ getValue }) => formatCurrency(getValue<number>()),
         header: () => 'Rate',
-        id: 'rate',
         size: 180,
       },
       {
         accessorKey: 'budget',
         cell: ({ getValue }) => formatCurrency(getValue<number>()),
         header: () => 'Budget',
-        id: 'budget',
         size: 180,
       },
       {
         accessorKey: 'fee',
         cell: ({ getValue }) => formatCurrency(getValue<number>()),
         header: () => 'Fee',
-        id: 'fee',
         size: 180,
       },
       {
         accessorKey: 'used',
         header: () => 'Used',
-        id: 'used',
         size: 180,
       },
       {
         accessorKey: 'notes',
         header: () => 'Notes',
-        id: 'notes',
         size: 180,
       },
     ],
@@ -274,42 +201,19 @@ const ShadcnTable = () => {
   }
 
   const [expanded, setExpanded] = React.useState<ExpandedState>(true)
-  const [columnOrder, setColumnOrder] = React.useState<string[]>(() =>
-    columns.map((c) => c.id!)
-  )
+
   const table = useReactTable({
     data,
     columns,
     state: {
-      expanded: true,
-      columnPinning: { left: ['wbs'] },
-      columnOrder: columnOrder,
+      expanded,
     },
     onExpandedChange: setExpanded,
     getSubRows: (row) => row.subRows,
     getCoreRowModel: getCoreRowModel(),
     getExpandedRowModel: getExpandedRowModel(),
     columnResizeMode: 'onChange',
-    onColumnOrderChange: setColumnOrder,
   })
-
-  // reorder columns after drag & drop
-  function handleDragEnd(event: DragEndEvent) {
-    const { active, over } = event
-    if (active && over && active.id !== over.id) {
-      setColumnOrder((columnOrder) => {
-        const oldIndex = columnOrder.indexOf(active.id as string)
-        const newIndex = columnOrder.indexOf(over.id as string)
-        return arrayMove(columnOrder, oldIndex, newIndex) //this is just a splice util
-      })
-    }
-  }
-
-  const sensors = useSensors(
-    useSensor(MouseSensor, {}),
-    useSensor(TouchSensor, {}),
-    useSensor(KeyboardSensor, {})
-  )
 
   return (
     <div className="p-2">
@@ -342,62 +246,112 @@ const ShadcnTable = () => {
         </DropdownMenu>
       </div>
       <div className="h-2" />
-      <DndContext
-        collisionDetection={closestCenter}
-        modifiers={[restrictToHorizontalAxis]}
-        onDragEnd={handleDragEnd}
-        sensors={sensors}
+      <Table
+        //IMPORTANT: pin and resize feature!
+        style={{
+          width: table.getTotalSize(),
+        }}
       >
-        <Table
-          //IMPORTANT: pin and resize feature!
-          style={{
-            width: table.getTotalSize(),
-          }}
-        >
-          <TableHeader>
-                      {table.getHeaderGroups().map((headerGroup) => {
-                        return (
-                          <TableRow key={headerGroup.id}>
-                              <SortableContext
-                                  items={columnOrder}
-                                  strategy={horizontalListSortingStrategy}
-                              >
-                              {headerGroup.headers.map((header) => {
-                                  return (
-                                    <TableHeaderWapper key={header.id} header={header} />
-                                  )
-                              })}
-                              </SortableContext>
-                          </TableRow>
-                        )
-                      })}
-                    </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows.map((row) => {
-              return (
-                <TableRow key={row.id}>
-                  {row.getVisibleCells().map((cell) => {
-                    const { column } = cell
-                    return (
-                      <TableCell
-                        className="bg-white"
-                        key={cell.id}
-                        //IMPORTANT: pin feature!
-                        style={{ ...getCommonPinningStyles(column) }}
-                      >
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
-                        )}
-                      </TableCell>
-                    )
-                  })}
-                </TableRow>
-              )
-            })}
-          </TableBody>
-        </Table>
-      </DndContext>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                const { column } = header
+                return (
+                  <TableHead
+                    colSpan={header.colSpan}
+                    className="bg-white"
+                    key={header.id}
+                    style={{ ...getCommonPinningStyles(column) }}
+                  >
+                    <div className="whitespace-nowrap">
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}{' '}
+                    </div>
+                    {/* pin controls start */}
+                    {!header.isPlaceholder && header.column.getCanPin() && (
+                      <div className="flex gap-1 justify-center">
+                        {header.column.getIsPinned() !== 'left' ? (
+                          <button
+                            className="border rounded px-2"
+                            onClick={() => {
+                              header.column.pin('left')
+                            }}
+                          >
+                            {'<='}
+                          </button>
+                        ) : null}
+                        {header.column.getIsPinned() ? (
+                          <button
+                            className="border rounded px-2"
+                            onClick={() => {
+                              header.column.pin(false)
+                            }}
+                          >
+                            X
+                          </button>
+                        ) : null}
+                        {header.column.getIsPinned() !== 'right' ? (
+                          <button
+                            className="border rounded px-2"
+                            onClick={() => {
+                              header.column.pin('right')
+                            }}
+                          >
+                            {'=>'}
+                          </button>
+                        ) : null}
+                      </div>
+                    )}
+                    {/* pin controls finish */}
+
+                    {/* resize controls start */}
+                    <div
+                      {...{
+                        onDoubleClick: () => header.column.resetSize(),
+                        onMouseDown: header.getResizeHandler(),
+                        onTouchStart: header.getResizeHandler(),
+                        className: `resizer ${
+                          header.column.getIsResizing() ? 'isResizing' : ''
+                        }`,
+                      }}
+                    />
+                    {/* resize controls finish */}
+                  </TableHead>
+                )
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows.map((row) => {
+            return (
+              <TableRow key={row.id}>
+                {row.getVisibleCells().map((cell) => {
+                  const { column } = cell
+                  return (
+                    <TableCell
+                      className="bg-white"
+                      key={cell.id}
+                      //IMPORTANT: pin feature!
+                      style={{ ...getCommonPinningStyles(column) }}
+                    >
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  )
+                })}
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
       <div>
         <Pagination>
           <PaginationContent>
